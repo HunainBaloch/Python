b2461b6dc5af95884b3ad6b0af861600b7dfff3c
Hunain Ali <119546006+HunainBaloch@users.noreply.github.com>
2024-06-16 16:17:12 +0500

    Var

--- Variables & Strings/Var.py
+++ Variables & Strings/Var.py
@@ -1,2 +1,15 @@
 my_name = "Hunain Ali"
+# print(my_name)
+
+# uni_name = "Nation University of Modern Languages"
+
+# print(uni_name)
+
+Age = 22
+graduate = True
+e_age = 22.5
+
 print(my_name)
+print(Age)
+print(graduate)
+print(e_age)
---
+++ Variables & Strings/exercise1.py
@@ -0,0 +1,5 @@
+person_name = "Tony Stark"
+person_age = 51
+isGeniue = True
+
+print(person_name,person_age,isGeniue)
